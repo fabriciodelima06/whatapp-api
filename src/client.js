f7d9aeb2379ee21e61b09a3ff90f07566aed59a6
unknown <fabriciodelima06@gmail.com>
2023-10-05 03:40:02 -0300

    first commit

---
+++ src/client.js
@@ -0,0 +1,62 @@
+import parsePhoneNumber, { isValidPhoneNumber } from "libphonenumber-js"
+import { create, SocketState } from "venom-bot"
+
+class Client {
+    client
+    connected
+    statusSession
+    qr
+
+    get isConnected() {
+        return this.connected
+    }
+
+    get qrCode() {
+        return this.qr
+    }
+
+    get status() {
+        return this.statusSession
+    }
+
+    constructor(sessionName) {
+        const qr = base64Qr => this.qr = base64Qr
+
+        const status = statusSession => {
+            //isLogged || notLogged || browserClose || qrReadSuccess || 
+            //qrReadFail || autocloseCalled || desconnectedMobile || deleteToken || 
+            //chatsAvailable || deviceNotConnected || serverWssNotConnected || 
+            //noOpenBrowser || initBrowser || openBrowser || connectBrowserWs || 
+            //initWhatsapp || erroPageWhatsapp || successPageWhatsapp || waitForLogin || 
+            //waitChat || successChat
+
+            this.statusSession = statusSession
+            this.connected = ['isLogged', 'qrReadSuccess', 'chatsAvailable', 'successChat'].includes(statusSession)
+      
+        }
+
+        const start = client => {
+            this.client = client
+            client.onStateChange(state => {
+                this.connected = state === SocketState.CONNECTED
+            })
+        }
+
+        create(sessionName, qr, status, { logQR: false })
+            .then(client => start(client))
+            .catch(error => console.log(error))
+    }
+
+    async sendText(to, body) {
+        if (!isValidPhoneNumber(to, 'BR')) {
+            throw new Error('This number is not valid!')
+        }
+
+        let phoneNumber = parsePhoneNumber(to, 'BR')?.format('E.164')?.replace('+', '')
+        phoneNumber = `${to}@c.us`
+        await this.client.sendText(phoneNumber, body)
+    }
+
+}
+
+export default Client
---
+++ src/server.js
@@ -0,0 +1,54 @@
+import fastify from 'fastify'
+import Client from './Client'
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const app = fastify()
+app.register(require('@fastify/cors'), {
+    origin: '*',
+    methods: 'GET,PUT,POST,DELETE,OPTIONS',
+  })
+
+let clientSession = {}
+
+app.post('/status', async (request, reply) => {
+    reply.header("Access-Control-Allow-Origin", "*");
+
+    const { sessionName } = request.body
+    if (!clientSession[sessionName])
+        clientSession[sessionName] = new Client(sessionName)
+
+    const client = clientSession[sessionName]
+
+    while (true) {
+        await sleep(1000)
+        if (client.qrCode || client.connected) break
+    }
+
+    reply.send({
+        qr_code: client.qrCode,
+        connected: client.isConnected,
+        status: client.status,
+    })
+
+})
+
+app.post('/send', (request, reply) => {
+
+    const { sessionName, number, message } = request.body
+
+    clientSession[sessionName].sendText(number, message)
+        .then(() => {
+            return reply.send()
+        })
+        .catch(error => {
+            return reply.status(500).send({ status: 'error', message: error })
+        })
+})
+
+app.listen({
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 3333
+}).then(() => {
+    console.log('HTTP Server Running')
+})
