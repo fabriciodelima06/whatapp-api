8528a73d0de66c902620487549484c45117b6448
Fabrício Aniceto de Lima <fabricio.lima@emap.ma.gov.br>
2023-10-06 16:28:50 -0300

    config chorme

--- render-build.sh
+++ render-build.sh
@@ -8,7 +8,7 @@ if [ ! -d $STORAGE_DIR/chrome ]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  wget -P ./https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
   rm ./google-chrome-stable_current_amd64.deb
   cd $HOME/project/src # Make sure we return to where we were
@@ -16,9 +16,7 @@ else
   echo "...Using Chrome from cache"
 fi
 
-cd $STORAGE_DIR
-$ls
-echo $__dirname
+echo -d $STORAGE_DIR/chrome/opt/google/chrome
 
 # be sure to add Chromes location to the PATH as part of your Start Command
 export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
